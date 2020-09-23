864a841abd3b86ee5ccc1629e7017686cb057415
mbartlett01 <67392004+mbartlett01@users.noreply.github.com>
2020-09-23 10:28:07 -0500

    Added HammingDist initialization

--- HammingDist.java
+++ HammingDist.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
+
 public class HammingDist
 {
+	//Class variables
+	private static ArrayList<String> cityCodes = new ArrayList<String>();
+	private static final String normanCode = "NRMN";
+	private static final String fileName = "Mesonet.txt";
 	
+	//Instance variables
+	private String city1;
+	private String city2;
+	private int city1Dist;
+	private int city2Dist;
+	
+	
+	public HammingDist(String city1, String city2)
+	{
+		this.city1 = city1;
+		this.city2 = city2;
+		
+		//Read in the city codes
+		readInCodes();
+	}
 }
